01491481f34d79446e418f1a1b3282d60badeed6
Alexander Novikau <ramram81ta@gmail.com>
2020-01-13 16:59:56 +0300

    added statistics of label

--- DNAstatistics.go
+++ DNAstatistics.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	tr, trReplay, zond, zondReplay, fam, alk, ln, allSacses, all, reBuild int
+	tr, trReplay, zond, zondReplay, fam, alk, ln, allSacses, all, reBuild, rox, hex, joe, cy5, tmr, r6g int
 )
 
 func main() {
@@ -35,7 +35,9 @@ func main() {
 	statistic(counts, countsDubl)
 	fmt.Printf(" Всего синтезировано %d\n Всего синтезированo с первого раза %d\n Синтезировано повторно %d\n", all, allSacses, reBuild)
 	fmt.Printf(" Fam %d\n Другие красители %d\n Зонды %d\n Зонд повторно %d\n LNA %d\n Очистка на картриджах %d\n Очистка на картриджах повторно %d\n", fam, alk, zond, zondReplay, ln, tr, trReplay)
+	fmt.Printf("  ROX d\n HEX %d\n JOE %d\n, Cy5 %d\n, TMR %d\n, R6G %d\n, Другие красители %d\n", rox, hex, joe, cy5, tmr, r6g, alk-rox-hex-joe-cy5-tmr-r6g)
 }
+
 func filesNames(in *os.File) []string {
 	var (
 		folder string
@@ -83,13 +85,32 @@ func countLines(f *os.File, counts map[string]int, countsDubl map[string]int, ar
 func statistic(counts map[string]int, countsDubl map[string]int) {
 	var dubl int
 	for line, n := range counts {
+		line = strings.ToUpper(line)
 		if strings.Contains(line, "[5]") {
 			fam++
 		}
 		if strings.Contains(line, "[7]") {
 			alk++
+			if strings.Contains(line, "ROX") {
+				rox++
+			}
+			if strings.Contains(line, "HEX") {
+				hex++
+			}
+			if strings.Contains(line, "JOE") {
+				joe++
+			}
+			if strings.Contains(line, "CY5") {
+				cy5++
+			}
+			if strings.Contains(line, "TMR") {
+				tmr++
+			}
+			if strings.Contains(line, "R6G") {
+				r6g++
+			}
 		}
-		if (strings.Contains(line, "-s") || strings.Contains(line, "-d")) && (strings.Contains(line, "[7]") || strings.Contains(line, "[5]")) {
+		if (strings.Contains(line, "-S") || strings.Contains(line, "-D")) && (strings.Contains(line, "[7]") || strings.Contains(line, "[5]")) {
 			zond++
 			if n != 1 {
 				zondReplay += (n - 1)
